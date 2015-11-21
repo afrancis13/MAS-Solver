bde0c0366e1caa24b7807b731adb0852c060c791
Alex Francis <afrancis@berkeley.edu>
2015-11-21 00:28:19 -0800

    Add parser, file hierarchy

---
+++ parser/test_parser.py
@@ -0,0 +1,36 @@
+import unittest
+
+from parser import Parser
+
+
+class ParserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_input_one = 'input_files/test_input_one.in'
+        self.test_input_two = 'input_files/test_input_two.in'
+        self.test_cancer = 'input_files/test_cancer.in'
+        self.test_parser_one = Parser(self.test_input_one)
+        self.test_parser_two = Parser(self.test_input_two)
+        self.test_parser_cancer = Parser(self.test_cancer)
+
+    def test_matrix_generator(self):
+        '''
+        Tests three input files for a correctly generated matrix.
+        The third test checks if the validator is working correctly,
+        so we check if a ValueError is appropriately raised.
+        '''
+        observed_one = self.test_parser_one.generate_matrix()
+        observed_two = self.test_parser_two.generate_matrix()
+        expected_one = [[0, 0, 1, 0],
+                        [0, 0, 0, 1],
+                        [0, 1, 0, 1],
+                        [1, 0, 0, 0]]
+        expected_two = [[1 for i in range(15)] for j in range(15)]
+
+        self.assertEqual(observed_one, expected_one)
+        self.assertEqual(observed_two, expected_two)
+
+        self.assertRaises(self.test_parser_cancer.generate_matrix(), ValueError)
+
+if __name__ == '__main__':
+    unittest.main()
