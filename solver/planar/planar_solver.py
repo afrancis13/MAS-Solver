e5e343ed2add74bd1dcaeb7779445239b212c367
Alex Francis <afrancis@berkeley.edu>
2015-11-22 18:26:32 -0800

    Add planarity check

--- solver/planar/planar_solver.py
+++ solver/planar/planar_solver.py
@@ -1,3 +1,6 @@
+import planarity
+
+
 class PlanarSolver(object):
 
     def __init__(self, adj_matrix):
@@ -10,7 +13,10 @@ class PlanarSolver(object):
         1. If you have a graph G = (V, E) that is connected and has
            |V| > 2, then if |E| > 3|V| - 6, G is not planar.
         2. Check if graph G has K(5) or K(3,3) as minors,
-           return True /False on planarity and nodes of "bad_minor"
+           return True /False on planarity and nodes of "bad_minor."
+           I used an open source project called planarity to do this -
+           hopefully their code works.
+           https://github.com/hagberg/planarity
         """
         num_vertices = len(self.adj_matrix)
         outgoing_edges = [node.count(1) for node in self.adj_matrix]
@@ -18,7 +24,13 @@ class PlanarSolver(object):
         if num_edges > 3 * num_vertices - 6:
             return False
         else:
-            return True  # Replace with an appropriate checking function
+            edges = []
+            for node in self.adj_matrix:
+                for adj_node in node:
+                    if self.adj_matrix[node][adj_node] == 1:
+                        edges.append((node, adj_node))
+            graph = planarity.PGraph(edges)
+            return graph.is_planar()
 
     def maximum_acyclic_subgraph(self):
         if not self.is_planar():
